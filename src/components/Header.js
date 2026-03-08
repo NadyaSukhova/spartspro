e4290282e90920d8b6762850610f7120cb8b081b
Nadya Sukhova <suhova1313@gmail.com>
2026-03-09 02:02:07 +0300

    fixed import for netlify

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import logo from "../assets/logo.png";
 import letterS from "../assets/letter-S.png";
-import partspro from "../assets/partspro.png";
+import otherLetters from "../assets/partspro.png";
 import menu from "../assets/menu.png";
 import close from "../assets/close.png";
 import styles from './Header.module.css';
@@ -11,7 +11,7 @@ export function Header() {
       <div class="${styles['company-name']}">
         <img src="${logo}" alt="логотип" class="${styles.logo}">
         <img src="${letterS}" alt="заглавная буква названия компании" class="${styles['first-letter']}">
-        <img src="${partspro}" alt="остальные буквы названия компании">
+        <img src="${otherLetters}" alt="остальные буквы названия компании">
       </div>
       <button class="${styles['menu-button']}"><img src="${menu}"/></button>
        <nav class="${styles.menu}">
